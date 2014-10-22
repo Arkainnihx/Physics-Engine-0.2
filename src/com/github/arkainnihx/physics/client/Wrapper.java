59d9a38fb2b826b6e7ebfc1f723c5f4de2296882
Arkainnihx <william.jcs@gmail.com>
2014-10-22 14:00:13 +0100

    Created a basic test gui using GuiFrame.

--- src/com/github/arkainnihx/physics/client/Wrapper.java
+++ src/com/github/arkainnihx/physics/client/Wrapper.java
@@ -4,21 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import com.github.arkainnihx.physics.client.gui.GuiFrame;
 import com.github.arkainnihx.physics.library.Universe;
 
 public class Wrapper {
 
 	public static void main(String[] args) throws NumberFormatException, IOException, InterruptedException {
 		Universe myUniverse = new Universe(2);
+		GuiFrame myGuiFrame = new GuiFrame();
+		myGuiFrame.setVisible(true);
 		Universe.listShapes();
 		myUniverse.addShape();
 		myUniverse.startSim();
+		
 		try {
 		    Thread.sleep(10000);                 
 		} catch(InterruptedException ex) {
 		    Thread.currentThread().interrupt();
 		}
 		myUniverse.stopSim();
+		System.exit(0);
 	}
 	
 	public static String getUserInput() throws IOException {
--- src/com/github/arkainnihx/physics/client/gui/GuiFrame.java
+++ src/com/github/arkainnihx/physics/client/gui/GuiFrame.java
@@ -1,8 +1,38 @@
 package com.github.arkainnihx.physics.client.gui;
 
+import java.awt.BorderLayout;
+import java.awt.GraphicsEnvironment;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class GuiFrame extends JFrame {
-
+	int screenWidth = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getWidth();
+	int screenHeight = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getHeight();
+	int frameWidth = 900;
+	int frameHeight = 600;
+	
+	public GuiFrame() {
+		JPanel contentPane = new JPanel(new BorderLayout());
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		JMenu simulationMenu = new JMenu("Simulation");
+		JMenuItem quitItem = new JMenuItem("Quit");
+		
+		this.setBounds((screenWidth-frameWidth)/2, (screenHeight-frameHeight)/2, frameWidth, frameHeight);
+		
+		
+		fileMenu.add(quitItem);
+		
+		menuBar.add(fileMenu);
+		menuBar.add(simulationMenu);
+		
+		this.setJMenuBar(menuBar);
+		this.setContentPane(contentPane);
+	}
+	
 }
